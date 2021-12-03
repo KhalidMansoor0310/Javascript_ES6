c2fe601150a366d5d9b374c489631c06c46a27af
KhalidMansoor0310 <khalidmansoor725@gmail.com>
2021-12-03 09:12:15 -0800

    es6 documentation

---
+++ js/js_es6.js
@@ -0,0 +1,5 @@
+// let vs var vs const 
+// var declarations are globally scoped or function scoped 
+// while let and const are block scoped. var variables can be updated
+// and re-declared within its scope; let variables can be updated but not re-declared; 
+// const variables can neither be updated nor re-declared. They are all hoisted to the top of their scope.
