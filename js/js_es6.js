c930853b659f636cc9b46ecac7a046b1b12014db
KhalidMansoor0310 <khalidmansoor725@gmail.com>
2021-12-03 09:17:30 -0800

    description plus code

--- js/js_es6.js
+++ js/js_es6.js
@@ -3,3 +3,18 @@
 // while let and const are block scoped. var variables can be updated
 // and re-declared within its scope; let variables can be updated but not re-declared; 
 // const variables can neither be updated nor re-declared. They are all hoisted to the top of their scope.
+
+var a = 10
+
+console.log(`value of a = ${a}`)
+
+var a = 20
+
+console.log(a)
+
+// let 
+
+let b = 10
+console.log(b)
+
+let b = 20 //will give an error its because it has declared once
